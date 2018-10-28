f53eb979e0a505631cb8d5a2a203292b3ee593ce
admindev <admindev>
2018-10-29 00:02:42 +0300

    *

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import './cssEntry/cssEntry';
 
+import './satellites/Satellites';
+import { satellites } from './satellites/Satellites';
+
 class ExtendedFilter {
 
     constructor(props) {
@@ -15,6 +18,8 @@ class ExtendedFilter {
             acqdate: 'Дата'
         };
 
+        this._setSatellites();
+
         this.state = this._setState();
     }
 
@@ -23,9 +28,31 @@ class ExtendedFilter {
         this._applyExtendedHeaders();
     }
 
+    _setSatellites() {
+
+        this._satellites = [];
+
+        satellites.ms.forEach(item => {
+            this._satellites.push(item);
+        });
+        satellites.pc.forEach(item => {
+            this._satellites.push(item);
+        });
+    }
+
     _setState() {
 
         
+    }
+
+    _getSatelliteList(cache) {
+        return cache.map((x) => {
+            const {id, name,} = x;
+            return `<div class="satellite-col">
+                        <input type="checkbox" id="sat_${id}" value="${id}" />
+                        <label for="sat_${id}">${name}</label>
+                    </div>`;
+        }).join('');
     }
 
     _applyExtendedHeaders() {
@@ -40,22 +67,43 @@ class ExtendedFilter {
 
         this._setHtmlContent(headerName);
         this._addEventListeners(headerName);
+        this._addSpecificEventListeners(headerName);
     }
 
     _setHtmlContent(headerName) {
 
         const headerElement = document.querySelector('#results-header-' + headerName);
         
-        headerElement.innerHTML = (
-            `<span class="filterable-header">${this._fieldNames[headerName]}</span>
-            <i class="table-list-sort"></i>`
+        let innerContent = '';
+
+        if (headerName === 'platform') {
+
+            innerContent = `
+            <div style="display: none;" class="togglable-content filterable-satellites-container">
+                <fieldset class="search-options-satellites-ms">
+                    ${this._getSatelliteList(this._satellites)}
+                </fieldset>
+                <div class="apply ">Применить</div>
+            </div>`;
+        }
+
+        let headerContent = (
+            `<div class="filterable-field-container">
+                <span class="filterable-header">${this._fieldNames[headerName]}</span>
+                <i class="table-list-sort"></i>
+                ${innerContent}
+            </div>`
         );
+
+        
+
+        headerElement.innerHTML = headerContent;
     }
 
     _addEventListeners(headerName) {
 
-        const headerElement = document.querySelector('#results-header-' + headerName + ' > span');
-        const sortElement = document.querySelector('#results-header-' + headerName + ' > i');
+        const headerElement = document.querySelector('#results-header-' + headerName + ' span');
+        const sortElement = document.querySelector('#results-header-' + headerName + ' i');
 
         headerElement.addEventListener('mouseover', this._onMouseOver.bind(this, headerName));
         headerElement.addEventListener('mouseout', this._onMouseOut.bind(this));
@@ -63,6 +111,15 @@ class ExtendedFilter {
         sortElement.addEventListener('mouseout', this._onMouseOut.bind(this));
 
         headerElement.addEventListener('click', this._onClick.bind(this, headerName));
+    }
+
+    _addSpecificEventListeners(headerName) {
+
+        if (headerName === 'platform') {
+            const applyButton = document.querySelector('#results-header-platform .apply');
+
+            applyButton.addEventListener('click', this._onApplyClick.bind(this));
+        }
     }
 
     _onMouseOver(headerName, e) {
@@ -97,10 +154,26 @@ class ExtendedFilter {
     _onClick(headerName, e) {
 
         const {target} = e;
-        
-        if (target) {
+
+        const hasActiveClass = target.classList.contains('active');
+        const parentNode = target.parentNode;
+        const filterContainer = parentNode.querySelector('.togglable-content');
+
+        if (!hasActiveClass) {
             target.classList.add('active');
+            filterContainer.style.display = 'block';
         }
+        else {
+            target.classList.remove('active');
+            filterContainer.style.display = 'none';
+        }
+    }
+
+    _onApplyClick(e) {
+
+        const togglableContainer = document.querySelector('#results-header-platform .togglable-content');
+
+        togglableContainer.style.display = 'none';
     }
 
 }
